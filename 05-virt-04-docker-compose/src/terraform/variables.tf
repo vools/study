0a9008e519fff07e816aa45e21a773d96d2c0c40
vools <68219914+vools@users.noreply.github.com>
2022-01-30 14:12:52 +0300

    Исправил конфиги terraform.

--- 05-virt-04-docker-compose/src/terraform/variables.tf
+++ 05-virt-04-docker-compose/src/terraform/variables.tf
@@ -1,17 +1,17 @@
 # Заменить на ID своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_cloud_id" {
-  default = "b1gu1gt5nqi6lqgu3t7s"
+  default = "b1gr10h2fjghhn1nck57"
 }
 
 # Заменить на Folder своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_folder_id" {
-  default = "b1gaec42k169jqpo02f7"
+  default = "b1gh195pf07mk83sti3g"
 }
 
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
 variable "centos-7-base" {
-  default = "fd8ft6norj68lo29qlpi"
+  default = "fd8kddmpep87oi5g3qc2"
 }
